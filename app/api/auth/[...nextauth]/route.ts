df065270bc3c26b303f4614886ffcf297b7a8c9b
aryansri-19 <aryansri.19@gmail.com>
2024-02-17 20:28:59 +0530

    husky

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth/next";
 
 import { authOptions } from "@/lib/authConfig";
 
-const { handler, auth } = NextAuth(authOptions);
+const { handler } = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, auth }
+export { handler as GET, handler as POST }
